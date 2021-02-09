8e615be7f97d05bb773e471e56c66106773c4e00
Eunjung Jo <35217620+comexpert1@users.noreply.github.com>
2021-02-09 16:07:09 +0900

    Change the message text

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello EJ World!!"}
+		res := &response{Message: "Welcome to EJ World!!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
